4ba27f8831b79eaaa453906769f7a0c9b8ae5a4d
Valentin <V.Kovalenko.prog@gmail.com>
2022-06-29 11:58:46 +0300

    add header and cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Cards.module.css'
+
+const Cards = () => {
+  return (
+    <section className={styles.wrapper}>
+      some content
+    </section>
+  );
+};
+
+export default Cards;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css'
+import logo from '../assets/img/logo.svg'
+// import { ReactComponent as Logo } from '../assets/img/logo.svg'
+
+
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.content}>
+        {/* <button>Button</button> */}
+        <img className={styles.logo} src={logo}/>
+        {/* <Logo width='20px' height='20px'/> */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
