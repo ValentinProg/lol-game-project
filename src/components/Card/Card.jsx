9e2a44a52b0bf36bac1692a15eaf9ba8bab84850
Valentin <V.Kovalenko.prog@gmail.com>
2022-06-30 19:53:28 +0300

    add cards image

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,15 +3,38 @@ import styles from './Card.module.css'
 
 
 const Card = (props) => {
-    console.log(props.champion.tags);
+  console.log(props.champion.id);
+  
+//   // Если с кавычкой, то сделать после кавычки с маленькой буквы
 
-    return (
-        <div className={styles.wrapper} >
+//   // name . ` " "
+
+//   const name = props.champion.name
+  
+//   const formatName = () => {
+//     if (name.includes("\'")) {
+//       const replaced = name.replace(/[']/g, '')
+//       const firstLetter = replaced.slice(0,1)
+//       const word = replaced.slice(1).toLowerCase()
+//       return `${firstLetter}${word}`
+//     } else {
+//       return name.replace(/[\s.]/g, '')
+//     }
+//   }
+
+//   formatName()
+      
+  return (
+    <div className={styles.wrapper} >
+        <div className={styles.content}>
             <h2>{props.champion.name}</h2>
             <span>{props.champion.tags[0]} {props.champion.tags[1]}</span>
             <p>{props.champion.title}</p>
         </div>
-    )
+        <img className={styles.img} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champion.id}_0.jpg`} alt="" />
+        {/* <div className={styles.darkEnd} ></div> */}
+    </div>
+  )
 }
 
 export default Card
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Card from '../Card/Card';
-import styles from './Cards.module.css'
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import Card from "../Card/Card";
+import styles from "./Cards.module.css";
+import axios from "axios";
 
 const Cards = () => {
-  const [champions, setChampions] = useState([]) 
+  const [champions, setChampions] = useState([]);
 
   // const getChampions = async () => {
   //   await fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
@@ -13,24 +12,24 @@ const Cards = () => {
   //   .then((data) => {
   //     console.log(data);
   //   })
-  // } 
+  // }
+
+  // api_key=RGAPI-34fd58b9-503d-45aa-889f-12e79562406c
   
   const getChampions = async () => {
-    await axios.get('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
-    .then(response => setChampions(Object.values(response.data.data)))
-  } 
+    await axios
+      .get("http://ddragon.leagueoflegends.com/cdn/12.12.1/data/en_US/champion.json")
+      .then((response) => setChampions(Object.values(response.data.data)));
+  };
 
   useEffect(() => {
-    getChampions()
-  }, [])
+    getChampions();
+  }, []);
 
- 
   return (
     <section className={styles.wrapper}>
-      {champions.map(champion => {
-        return (
-          <Card champion={champion} key={champion.key}/>
-        )
+      {champions.map((champion) => {
+        return <Card champion={champion} key={champion.key} />;
       })}
     </section>
   );
