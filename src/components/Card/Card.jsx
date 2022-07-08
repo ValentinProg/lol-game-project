f99cecfbc995535c59423c095893bacc47918a74
Valentin <V.Kovalenko.prog@gmail.com>
2022-07-08 16:43:38 +0300

    add cardBack

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Card.module.css'
-
+import sword from '../assets/img/sword.svg'
+import shield from '../assets/img/shield.svg'
+import star from '../assets/img/star.svg'
+import magic from '../assets/img/magic.svg'
 
 const Card = (props) => {
 
+  const [isCardFlipped, setIsCardFlipped] = useState(false)
+
+  const changeCardState = () => {
+    if (isCardFlipped) {
+      setIsCardFlipped(false)
+
+    } else {
+      setIsCardFlipped(true)
+    }
+  }
+
+  // Если мне нужно заменить значение на противоположное в случае true/false, тогда (prevState => !prevState)
+
+  // const [isCardFlipped, setIsCardFlipped] = useState(false)
+  // setIsCardFlipped((prevState) => !prevState)
+
+
   return (
-   <div className={styles.border}>
-    <div className={styles.wrapper}>
-        <div className={styles.content}>
-            <h2>{props.champion.name}</h2> 
-            <span className={styles.tags}>
-              {props.champion.tags.join(' ')}
-            </span> 
-            <p>{props.champion.title}</p>
-        </div>  
-        <img className={styles.img} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champion.id}_0.jpg`} alt="" />
-    </div>
+    <div className={styles.border}>
+      <div className={styles.wrapper} onClick={changeCardState}>
+
+      {
+        isCardFlipped
+        ? <div className={styles.contentBack}>
+            <img className={styles.sprite} src={`http://ddragon.leagueoflegends.com/cdn/12.12.1/img/champion/${props.champion.id}.png`}/>
+            <div>
+              <span className={styles.tags}>
+                <img className={styles.icons} src={sword}/> Attack: {props.champion.info.attack}
+              </span><br/>
+
+              <span className={styles.tags}>
+                <img className={styles.icons} src={shield}/>Defense: {props.champion.info.defense}
+              </span><br/>
+
+              <span className={styles.tags}>
+                <img className={styles.icons} src={star}/>Difficulty: {props.champion.info.difficulty}
+              </span><br/>
+              
+              <span className={styles.tags}>
+                <img className={styles.icons} src={magic}/>Magic: {props.champion.info.magic}
+              </span><br/>
+            </div>
+            <p className={styles.discription}>{props.champion.blurb}</p>
+          </div>  
+        : <>
+            <div className={styles.content}>
+              <h2>{props.champion.name}</h2> 
+              <span className={styles.tags}>
+                {props.champion.tags.join(' ')}
+              </span> 
+              <p>{props.champion.title}</p>
+            </div>  
+            <img className={styles.img} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champion.id}_0.jpg`} alt="" />
+          </>
+      }
+      </div>
     </div>
   )
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const Cards = () => {
   const [champions, setChampions] = useState([]);
+  console.log(champions);
 
   // const getChampions = async () => {
   //   await fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
