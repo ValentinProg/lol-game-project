7caf772df5dd31d590ae2136ae02009f2c7f7d30
Valentin <V.Kovalenko.prog@gmail.com>
2022-07-29 17:35:36 +0300

    change sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
 
 const Sidebar = (props) => {
-    return (
-        <div className={`${styles.sidebar} ${props.sidebar && `${styles.sidebar} ${styles.sidebarOpen}`}`} >
-            <div className={styles.content}>
-            <li>Mage</li>
-            <li>Warrrior</li>
-            <li>Assasion</li>
-            <li>Noob</li>
-            <li>Tank</li>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className={`${styles.sidebar} ${props.sidebar && `${styles.sidebar} ${styles.sidebarOpen}`}`}
+    >
+      <div className={styles.content}>
+        <li>Mage</li>
+        <li>Warrrior</li>
+        <li>Assasion</li>
+        <li>Noob</li>
+        <li>Tank</li>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
